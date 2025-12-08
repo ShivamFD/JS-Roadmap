e11bfab2fc16326c6172c545c405e80e8ec7152d
ShivamFD <akkivishwakarma52@gmail.com>
2025-12-08 09:02:07 +0530

    feat(day2) : added notes , tasks and solutions for operators

---
+++ Day-02-Operators/solutions/easyTaskSolutions.js
@@ -0,0 +1,48 @@
+// task01_addNumbers.js
+let a = 10, b = 20;
+console.log(a + b);
+
+// task02_remainder.js
+let a = 10, b = 3;
+console.log(a % b);
+
+// task03_stringToNumber.js
+let x = +"45";
+console.log(x, typeof x);
+
+// task04_incrementDecrement.js
+let x = 5;
+x++;
+x--;
+console.log(x);
+
+// task05_compareValues.js
+console.log(5 == "5");  
+console.log(5 === "5");
+
+// task06_logicalAnd.js
+let age = 25;
+console.log(age > 18 && age < 30);
+
+// task07_joinStrings.js
+let firstname = "Shivam";
+let lastname = "Verma";
+console.log(firstname + " " + lastname);
+
+// task08_typeofCheck.js
+console.log(typeof 10);
+console.log(typeof "test");
+console.log(typeof true);
+console.log(typeof {});
+console.log(typeof undefined);
+
+// task09_evenOrOdd.js
+let num = 10;
+console.log(num % 2 === 0 ? "Even" : "Odd");
+
+// task10_unaryNegative.js
+let x = -"10";
+console.log(x);
+
+
+
